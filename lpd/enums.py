c51cf177b135e02185934e9cabf4d4be1bba3ca9
Roy Sadaka <roy.sadaka@toluna.com>
2020-10-06 20:43:25 +0300

    enum auto usage

--- lpd/enums.py
+++ lpd/enums.py
@@ -1,14 +1,14 @@
-from enum import Enum
+from enum import Enum, auto
 
 class CallbackPhase(Enum): 
-    ON_TRAIN_BEGIN   = 0
-    ON_TRAIN_END     = 1
-    ON_EPOCH_BEGIN   = 2
-    ON_EPOCH_END     = 3
-    ON_BATCH_BEGIN   = 4
-    ON_BATCH_END     = 5
-    ON_TEST_BEGIN    = 6
-    ON_TEST_END      = 7
+    ON_TRAIN_BEGIN   = auto()
+    ON_TRAIN_END     = auto()
+    ON_EPOCH_BEGIN   = auto()
+    ON_EPOCH_END     = auto()
+    ON_BATCH_BEGIN   = auto()
+    ON_BATCH_END     = auto()
+    ON_TEST_BEGIN    = auto()
+    ON_TEST_END      = auto()
 
 class TrainerState(Enum):
     """
@@ -18,26 +18,26 @@ class TrainerState(Enum):
         TEST - when calling evaluate(...)
         EXTERNAL - all other states, for example, after valiadaion is done and before the next epoch train has begun
     """
-    EXTERNAL = 0
-    TRAIN    = 1
-    VAL      = 2 
-    TEST     = 3
+    EXTERNAL = auto()
+    TRAIN    = auto()
+    VAL      = auto() 
+    TEST     = auto()
 
 class StatsType(Enum):
-    TRAIN    = 0
-    VAL      = 1 
-    TEST     = 2
+    TRAIN    = auto()
+    VAL      = auto() 
+    TEST     = auto()
     def __str__(self):
         return self.name
 
 class MonitorType(Enum):
-    LOSS    = 0
-    METRIC  = 1
+    LOSS    = auto()
+    METRIC  = auto()
     def __str__(self):
         return self.name
 
 class MonitorMode(Enum):
-    MIN  = 0
-    MAX  = 1
+    MIN  = auto()
+    MAX  = auto()
     def __str__(self):
         return self.name
